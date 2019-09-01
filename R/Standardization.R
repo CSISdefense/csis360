441a1af0d6218b600948200abfda4d759825674b
Gregory Sanders <grysar@zort.net>
2019-09-01 17:48:19 -0400

    Dropping term num, standardizing on partial or complete termination.

--- R/Standardization.R
+++ R/Standardization.R
@@ -469,7 +469,12 @@ transform_contract<-function(
 
   #b_Term
   if("Term" %in% colnames(contract)){
-    contract$b_Term<-ifelse(contract$Term %in% c("Terminated",1),1,NA)
+    #Not sure why Term was swapped to binary, but fixing it.
+    if(!is.factor(contract$Term)) contract$Term<-factor(contract$Term)
+      levels(contract)<- list("Partial or Complete Termination"=c("Partial or Complete Termination","Terminated","1",1),
+                                   "Unterminated"=c("Unterminated","0",0))
+
+    contract$b_Term<-ifelse(contract$Term %in% c("Partial or Complete Termination","Terminated",1),1,NA)
     contract$b_Term[contract$Term %in% c("Unterminated",0)]<-0
     #Create a jittered version of Term for display purposes
     #Unlike geom_jitter, this caps values at 0 and 1
@@ -1452,14 +1457,6 @@ transform_contract<-function(
 
   if("cl_Days" %in% colnames(contract))
     contract$cl_Days<-arm::rescale(contract$l_Days)
-
-
-
-
-  if("TermNum" %in% colnames(contract))
-    contract$TermNum<-as.integer(as.character(factor(contract$Term,
-                                                     levels=c("Terminated","Unterminated"),
-                                                     labels=c(1,0))))
 
   if("Action_Obligation" %in% colnames(contract)){
     contract$ObligationWT<-contract$Action_Obligation
