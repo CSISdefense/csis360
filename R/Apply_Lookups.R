ec95fbfcb36ed908b0339dcec768f6e77ddbff95
Henry Carroll <HCarroll@csis.org>
2024-07-15 17:11:23 -0400

    HenrySharepointAdd
    Adding HHC Sharepoint access

--- R/Apply_Lookups.R
+++ R/Apply_Lookups.R
@@ -143,6 +143,9 @@ get_local_sharepoint_path<-function(site="DIIG - Documents"){
   local_path<-file.path("C:\\Users\\grego\\Center Strategic Intl Studies Inc CSIS",site)
   if(file.exists(local_path))
     return(local_path)
+  local_path<-file.path("C:\Users\HCarroll\Center Strategic Intl Studies Inc CSIS",site)
+  if(file.exists(local_path))
+    return(local_path)
   stop("Could not find local path. Update the list in Apply_Lookups.R")
 }
 
